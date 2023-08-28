95a2c8d50b692dfbf277fd1c2f83aec36e9b5b76
mahrokh osanlou <mahrokh.osanlou.dev@gmail.com>
2023-08-28 11:33:08 +0330

    weather forcast done

--- weather/src/app/modules/weather/components/weather-forcast/weather-forcast.component.ts
+++ weather/src/app/modules/weather/components/weather-forcast/weather-forcast.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
+import { MyErrorStateMatcher } from 'src/app/modules/shared/classes/error-state-matcher';
 
 @Component({
   selector: 'app-weather-forcast',
   templateUrl: './weather-forcast.component.html',
-  styleUrls: ['./weather-forcast.component.scss']
+  styleUrls: ['./weather-forcast.component.scss'],
 })
-export class WeatherForcastComponent {
+export class WeatherForcastComponent implements OnDestroy {
+  matcher = new MyErrorStateMatcher();
+  constructor(public weatherService: WeatherService) {}
 
+  ngOnDestroy(): void {
+    this.weatherService.$weatherResult.next(null);
+    this.weatherService.toShow = false;
+    this.weatherService.cityFormControl.reset();
+  }
 }
