b7e264b97e10f77ca45f6a3a03e90763b98d47d9
mahrokh osanlou <mahrokh.osanlou.dev@gmail.com>
2023-08-28 11:33:20 +0330

    weather list done

--- weather/src/app/modules/weather/components/weather-list/weather-list.component.ts
+++ weather/src/app/modules/weather/components/weather-list/weather-list.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProgressSpinnerMode } from '@angular/material/progress-spinner';
+import { WeatherService } from '../../services/weather.service';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-weather-list',
   templateUrl: './weather-list.component.html',
-  styleUrls: ['./weather-list.component.scss']
+  styleUrls: ['./weather-list.component.scss'],
 })
-export class WeatherListComponent {
+export class WeatherListComponent implements OnInit {
+  displayedColumns: string[] = ['name', 'country' ,  'temp', 'humidity', 'weatherCondition'];
+  mode: ProgressSpinnerMode = 'indeterminate';
 
+  constructor(public weatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    if (
+      !this.weatherService.weatherDataSource.data.length &&
+      localStorage.getItem('list')
+    ) {
+      this.weatherService.weatherDataSource = new MatTableDataSource<any>(
+        JSON.parse(localStorage.getItem('list') ?? '')
+      );
+    }
+  }
 }
